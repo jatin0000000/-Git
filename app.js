7b2dcb5071d3c39d90edee4e3ee9a7a259fa81ff
Jatin Singh <ja3singh99101@gmail.com>
2023-02-27 06:08:17 +0530

    changed lots

---
+++ app.js
@@ -0,0 +1,2 @@
+let j = "jatin";
+alert(j);
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let x = "jatin singh";
 document.write(`${x} is the best batsman`);
+document.write("<br>");
+
 
 let y = 5;
 let z = 6;
 
 document.write(y+z);
+
